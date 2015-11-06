ee04f95d4e12fa3144840385464dad38f32588bc
Rui <liurui666666@hotmail.com>
2015-11-06 09:52:46 +0800

    reviw this script

--- add_user.sh
+++ add_user.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-echo "****************************************************************"
-echo "**********This utility is to multitudinously add user.**********"
-echo "**********Usage : ./add_user  argument                **********"
-echo "**********Coder : liurui666666@hotmail.com            **********"
-echo "**********Date  : 2015/10/27                          **********"
-echo "****************************************************************"
+echo "################################################################"
+echo "##########This utility is to multitudinously add user.##########"
+echo "##########Usage : ./add_user  argument                ##########"
+echo "##########Coder : liurui666666@hotmail.com            ##########"
+echo "##########Date  : 2015/10/27                          ##########"
+echo "################################################################"
+
 
 echo -n "Enter the user file: "  
 read user
 echo -n "Enter the suffix of password:"
 read suffix
+echo -n "Some comments about this user: "
+read comment
+echo -n "Belong to group: "
+read group
+
+#此处需要判断是否已经存在该组
+if [ -n $group ]; then
+	
+	groupadd $group
+fi
 
 for username in `cat ./$user`
 do
-	if [ -n $username ]; then
+	if [ -n ${username} ]; then
 
-			useradd -m $username
+			useradd -m ${username} -c $comment -g $group  >> out.txt 2>&1 
 			echo 
-			echo $username"$suffix" | passwd --stdin $username
+			echo "$username$suffix" | passwd --stdin $username >>out.txt 2>&1
 			echo
-			echo "User $username's password was changed! "
 	else
 		echo "user is null!"	
 	fi
